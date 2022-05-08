c57cab493509045944041267a91c85280a76072f
InSun <aolgit@aol.com>
2022-05-08 09:33:40 +0900

    P9655/Cpp

---
+++ P9655.cpp
@@ -0,0 +1,28 @@
+// DP
+// https://www.acmicpc.net/problem/9655
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int N;
+    int DP [1001];
+    
+    DP[0] = 0;
+    DP[1] = 1;
+    DP[2] = 2;
+
+    cin >> N;
+    
+    for (int i = 3; i <= N; i++) {
+        DP[i] = min((DP[i - 1] + 1), DP[i - 3] + 1);
+    }
+
+    if (DP[N] % 2 == 1) {
+        cout << "SK";
+    } else {
+        cout << "CY";
+    }
+
+}
